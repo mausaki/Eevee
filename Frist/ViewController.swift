515031e40a7c8ea2f202310d2d0c1d6779beb152
Mau <rainna.saki@gmail.com>
2017-10-31 22:19:58 +0800

    Initial Commit

---
+++ Frist/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Frist
+//
+//  Created by mau on 2017/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
